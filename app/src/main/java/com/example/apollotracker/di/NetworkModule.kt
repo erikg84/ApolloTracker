f53451288f5e0dc975a24c8d203f421d72bd33e8
AndroidDevErik <ergutier@syr.edu>
2024-08-02 19:36:55 -0500

    added dependencies

--- app/src/main/java/com/example/apollotracker/di/NetworkModule.kt
+++ app/src/main/java/com/example/apollotracker/di/NetworkModule.kt
@@ -9,25 +9,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-object NetworkModule {
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideCoinPaprikaApi(retrofit: Retrofit): CoinPaprikaApi {
-        return retrofit.create(CoinPaprikaApi::class.java)
-    }
-
-    private const val BASE_URL = "https://api.coinpaprika.com/"
-}
+//@Module
+//@InstallIn(SingletonComponent::class)
+//object NetworkModule {
+//
+//    @Provides
+//    @Singleton
+//    fun provideRetrofit(): Retrofit {
+//        return Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//    }
+//
+//    @Provides
+//    @Singleton
+//    fun provideCoinPaprikaApi(retrofit: Retrofit): CoinPaprikaApi {
+//        return retrofit.create(CoinPaprikaApi::class.java)
+//    }
+//
+//    private const val BASE_URL = "https://api.coinpaprika.com/"
+//}
 
